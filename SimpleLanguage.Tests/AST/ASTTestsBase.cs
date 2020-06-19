15f431a2b8c8c950b27958ce6dc88731260637ac
tshkuro <shkuro_tanya@mail.ru>
2020-06-19 13:36:50 +0300

    Try to add ASTTestBase

---
+++ SimpleLanguage.Tests/AST/ASTTestsBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using SimpleLang;
+using SimpleLang.Visitors;
+using SimpleParser;
+using SimpleScanner;
+
+namespace SimpleLanguage.Tests.AST
+{
+    public class ASTTestsBase
+    {
+        protected Parser GenAST(string sourceCode)
+        {
+            ThreeAddressCodeTmp.ResetTmpName();
+            ThreeAddressCodeTmp.ResetTmpLabel();
+            SymbolTable.vars.Clear();   // oh yeah, all variables are stored in a static dict :D
+            var scanner = new Scanner();
+            scanner.SetSource(sourceCode, 0);
+            var parser = new Parser(scanner);
+            parser.Parse();
+            var fillParents = new FillParentsVisitor();
+            parser.root.Visit(fillParents);
+            return parser;
+        }
+    }
+}
---
+++ SimpleLanguage.Tests/AST/OptStatIfTrueTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SimpleLang;
+using SimpleLang.Visitors;
+
+namespace SimpleLanguage.Tests.AST
+{
+    [TestFixture]
+    internal class OptStatIfTrueTest : ASTTestsBase
+    {
+        OptStatIfTrue opt = new SimpleLang.Visitors.OptStatIfTrue();
+
+    }
+}
